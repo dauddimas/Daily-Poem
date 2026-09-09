9f5e04d41df861999c93ed0132f0c44f3f674706
dauddimas <dauddimasp@gmail.com>
2026-09-09 15:16:32 +0700

    Initial Commit

---
+++ Daily Poem/Daily_PoemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_PoemApp.swift
+//  Daily Poem
+//
+//  Created by Daud Dimas Prasetyo on 09/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Daily_PoemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
